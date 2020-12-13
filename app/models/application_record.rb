2207cf9ef469c09d0bc8ddf1610fe015228d676b
Kate Tester <katemorris@gmail.com>
2020-12-13 13:24:56 -0700

    Moves search method to application record as merchant will need it.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,15 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.search(params)
+    items = []
+    params.each do |param, value|
+      if param == 'created_at' || param == 'updated_at'
+        items << self.where("DATE(#{param}) = ?", "%#{value}%") if self.has_attribute?(param)
+      else
+        items << self.where("LOWER(#{param}) LIKE ?", "%#{value}%") if self.has_attribute?(param)
+      end
+    end
+    items.flatten
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,16 +7,4 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :unit_price, presence: true
-
-  def self.search(params)
-    items = []
-    params.each do |param, value|
-      if param == 'created_at' || param == 'updated_at'
-        items << Item.where("DATE(#{param}) = ?", "%#{value}%") if Item.has_attribute?(param)
-      else
-        items << Item.where("LOWER(#{param}) LIKE ?", "%#{value}%") if Item.has_attribute?(param)
-      end
-    end
-    items.flatten
-  end
 end
